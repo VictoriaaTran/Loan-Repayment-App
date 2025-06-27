d250eb53d5c900f1aa115e946fb51e9c59f64977
VictoriaaTran <victoriatrnn@gmail.com>
2025-06-27 16:30:26 -0700

    remove import

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import uuid
-# import plotly.express as px
-import matplotlib.pyplot as plt
 
 # set page config
 st.set_page_config(page_title="Loan Repayment Calculator", layout='wide')
