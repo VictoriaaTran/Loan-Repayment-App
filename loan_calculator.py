8e87ba3352a10b858a672540c4eb17ef82386b67
VictoriaaTran <victoriatrnn@gmail.com>
2025-06-27 16:12:40 -0700

    fix error

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 # import matplotlib.pyplot as plt
-import plotly.express as px
+# import plotly.express as px
 import uuid
-import numpy as np
 
 # set page config
 st.set_page_config(page_title="Loan Repayment Calculator", layout='wide')
